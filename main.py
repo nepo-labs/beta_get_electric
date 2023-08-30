51f6a969808ebb4e85f24ad1363d6ebc9f31c674
israelbenh463Ni <israelbenh@gmail.com>
2023-08-30 14:32:34 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import json
+import os
+import re
+from logging.config import dictConfig
+
+import flask
+
+from flask import Flask, request, render_template, send_from_directory
+from datetime import datetime, timedelta
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+
+app = flask.Flask(__name__)
+
+
+@app.route('/get-electric/terms-of-use',methods=['GET'])
+def terms_of_use():
+    return render_template('terms-of-use.html')
+
+@app.route('/get-electric/privacy-policy',methods=['GET'])
+def privacy_policy():
+    return render_template('privacy-policy.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
